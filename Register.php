d3cb1e5afb3dc9826b890b6a0d181eae30968191
Swastika-Giri <swastika.giri7@gmail.com>
2022-09-02 12:56:53 +0545

    Register.php

---
+++ Register.php
@@ -0,0 +1,45 @@
+<html>
+    <head> 
+        <title>  Registration form </title>
+    </head>
+    <body>
+        <form action=" " method="post">
+            <label for="name"> Enter your  Name</label>
+        <input type="text" name="name" id="name">
+        <br>
+        <br>
+        <label for="email">  Enter your Email</label>
+        <input type="text" name="email" id="email">
+        <br>
+        <br>
+        <label for="gender"> Gender</label>
+        select your gender<br>
+         male: <input type="radio" name="male" value="male">
+         female <input type="radio" name="female" value="female"><br></br>
+         <label for="education">Education</label>
+         select your education:<br>
+         <input type="checkbox" id="education" name="education" value="education">
+         <label for="slc">SLC pass </label><br>
+         <input type="checkbox" id="plus two" name="plus two">
+         <label for ="+2"> 12 pass</label><br>
+         <input type="checkbox" id="plus two" name="plus two">
+         <label for ="bachelor"> bachelor pass</label><br>
+         <label for "faculty"> choose your faculty</label>
+         <select name="faculty" id="faculty" value="faculty">
+             <optgroup label ="bca">
+                 <option 
+                 value="bca"> Bca</option>
+                 <option 
+                 value="bbm"> BBM</option>
+                 <option 
+                 value="bca"> BBA</option>
+</select>
+                 <br></br>
+                 Enter your password:<input type="password" name="password"><br></br>
+                 <input type="button" name="submit" value="Register">
+                 <div class="social-login">
+			
+        </form>
+            
+        
+    </body>
